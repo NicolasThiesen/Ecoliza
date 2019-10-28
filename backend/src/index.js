ef57e5c5511afcc61545155d85d4372c8ee51783
Victor Ohashi <victor.ohashi@gmail.com>
2019-10-28 08:12:04 -0300

    Removed node-modules package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const requireDir = require("require-dir");
-const cors = require("cors");
-const mongoose = require ('mongoose');
-const dotenv = require("dotenv");
+const express = require('express')
+const requireDir = require('require-dir')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-mongoose.connect(process.env.DATA_CONNECT,{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATA_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
-app.use(express.json());
+app.use(express.json())
 
-requireDir("./models")
+requireDir('./models')
 
-const Ponto = mongoose.model("Ponto");
+const Ponto = mongoose.model('Ponto')
 
 app.use(cors())
 
-app.use(require('./routes'));
+app.use(require('./routes'))
 
-app.listen(3333);
+app.listen(3333)
