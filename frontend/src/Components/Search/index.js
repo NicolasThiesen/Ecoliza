084881b7f6ede1c443ca39890849276e1d70a2bd
Victor Ohashi <victor.ohashi@gmail.com>
2019-10-30 08:09:55 -0300

    Refactor

--- frontend/src/Components/Search/index.js
+++ frontend/src/Components/Search/index.js
@@ -1,44 +1,54 @@
-import React,{useState} from 'react';
-import api from "../../services/api"
-import  './styles.css';
-import { display } from '@material-ui/system';
+import React, { useState } from 'react'
+import api from '../../services/api'
+import './styles.css'
 
 export default function Search() {
-    const [focus,setFocus] = useState(false);
-    const [data,setData] = useState([]); 
-    const [names,setNames] = useState([]);
+  const [focus, setFocus] = useState(false)
+  const [data, setData] = useState([])
+  const [names, setNames] = useState([])
 
-    const handleClick = event =>{
-        console.log("clicked_data");
-        
+  const handleClick = event => {
+    console.log('clicked_data')
+  }
+
+  const handleChange = async event => {
+    const name = event.target.value
+    if (name !== '') {
+      const res = await api.get(`/ponto/nome/${name}`)
+      setNames(res.data)
     }
+  }
 
-    const handleChange = async event => {
-        const name = event.target.value
-        if (name !== ""){
-          const res = await api.get(`/ponto/nome/${name}`)
-          setNames(res.data);
-          
-        }
-      };
-    
   return (
-    /* Barra de pesquisa de um ponto */
     <div className="search">
-        {/* Input para pesquisa de um ponto pelo nome */}
-        <input placeholder="Digite o Ponto" onChange={handleChange} onFocus={()=>{setFocus(true)}} onBlur={()=>{setFocus(!focus)}}></input>
-        {/* Botões para pesquisa de pontos pelo tipo de reciclagem */}
-        {focus && (
-            <div className="dropdown"> 
-                {names.map(suggestion =>{
-                return (
-                    <div key={suggestion._id}>
-                        <span onClick={()=>{handleClick(suggestion)}}>{suggestion.name}</span>
-                    </div>
-                    )
-                 })}
-            </div>
-        )}
+      <input
+        placeholder="Digite o Ponto"
+        onChange={handleChange}
+        onFocus={() => {
+          setFocus(true)
+        }}
+        onBlur={() => {
+          setFocus(!focus)
+        }}
+      ></input>
+
+      {focus && (
+        <div className="dropdown">
+          {names.map(suggestion => {
+            return (
+              <div key={suggestion._id}>
+                <span
+                  onClick={() => {
+                    handleClick(suggestion)
+                  }}
+                >
+                  {suggestion.name}
+                </span>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
-  );
+  )
 }
